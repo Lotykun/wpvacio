362197807e5a2f5a8cb515e2960fae5ca96091a5
jlotito <jlotito@opensistemas.com>
2014-12-22 17:15:02 +0100

    modificando y haciendo un tema desde cero

---
+++ wordpress/wp-content/themes/mitema/footer.php
@@ -0,0 +1,9 @@
+
+        </div> <!-- Fin de wrapper -->
+        <footer>
+            <p><?php bloginfo('name'); ?>, <?=date('Y');?>. By © LOTY, jlotito@opensistemas.com</p>
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
+</html>
+
---
+++ wordpress/wp-content/themes/mitema/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Registro del menú de WordPress
+add_theme_support( 'nav-menus' );
+if ( function_exists( 'register_nav_menus' ) ) {
+    register_nav_menus(
+        array(
+          'main' => 'Main'
+        )
+    );
+}
+if(function_exists('register_sidebar'))
+    register_sidebar(array(
+        'name' => 'Main Sidebar',
+        'before_widget' => '<hr>',
+        'after_widget' => '',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>',
+    ));
+//Habilitar thumbnails
+add_theme_support('post-thumbnails');
+set_post_thumbnail_size(150, 150, true);
+
+// Permitir comentarios encadenados
+function enable_threaded_comments(){
+    if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+       wp_enqueue_script('comment-reply');
+    }
+}
+add_action('get_header', 'enable_threaded_comments');
+
+
+?>
